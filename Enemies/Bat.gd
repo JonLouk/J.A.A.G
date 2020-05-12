38c937b00fef049bb9de723e9ce5b1cf7c9a53f8
whatevs666 <giannos007@gmail.com>
2020-05-12 12:58:29 +0300

    Hit Effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,14 +1,40 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+enum{
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
+
+var state = CHASE
 
 onready var stats = $Stats
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION* delta)
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			pass
+
+func seek_player():
+	pass
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
