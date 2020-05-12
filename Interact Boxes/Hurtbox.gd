a44f9a39731ca18bce1b5d7f3bcd77ca9d4ff2d7
whatevs666 <giannos007@gmail.com>
2020-05-12 16:44:53 +0300

    Player Health and Enemy Attack

--- Interact Boxes/Hurtbox.gd
+++ Interact Boxes/Hurtbox.gd
@@ -1,12 +1,38 @@
 extends Area2D
 
-export(bool) var show_hit = true
-
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit:
-		var effect = HitEffect.instance()
-		var main = get_tree().current_scene
-		main.add_child(effect)
-		effect.global_position = global_position
+var invincible = false setget set_invincible
+
+onready var timer = $Timer
+
+signal invisibility_started
+signal invisibility_ended
+
+func set_invincible(value):
+	invincible = value
+	if invincible == true:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("inincibility_ended")
+
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start(duration)
+
+
+func create_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+
+func _on_Timer_timeout():
+	self.invincible = false
+
+
+func _on_Hurtbox_invisibility_started():
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_invisibility_ended():
+	monitorable = true
