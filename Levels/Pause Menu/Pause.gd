a73bd6cca450c4f270ab0d2dbb824ece002ce8db
athamour1 <thanosmour.tk@gmail.com>
2020-05-28 14:18:43 +0300

    Main menu animation

--- Levels/Pause Menu/Pause.gd
+++ Levels/Pause Menu/Pause.gd
@@ -1,7 +1,20 @@
 extends Control
 
+var new_pause_state
+
 func _input(event):
 	if event.is_action_pressed("pause"):
-		var new_pause_state = not get_tree().paused
+		new_pause_state = not get_tree().paused
 		get_tree().paused = not get_tree().paused
 		visible = new_pause_state
+
+
+func _on_MainMenu_pressed():
+	get_tree().paused = not get_tree().paused
+	get_tree().change_scene("res://Main_Menu/Main_menu.tscn")
+
+
+func _on_Retry_pressed():
+	get_tree().paused = not get_tree().paused
+	visible = new_pause_state
+	get_tree().change_scene("res://Levels/World.tscn")
