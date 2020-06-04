d4c858da0ff86d405a0d344136b2e4e23fe8590f
athamour1 <thanosmour.tk@gmail.com>
2020-06-04 17:35:51 +0300

    Fade in effect in all menus

--- Main_Menu/SubMenus/SubMenu.gd
+++ Main_Menu/SubMenus/SubMenu.gd
@@ -1,7 +1,9 @@
 extends Control
 
 func _on_Button_pressed():
-#	$FadeIn.show()
-#	$FadeIn.fade_in()
-	get_tree().change_scene("res://Main_Menu/Main_menu.tscn")
+	$FadeIn.show()
+	$FadeIn.fade_in()
 
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$FadeIn.hide()
+	get_tree().change_scene("res://Main_Menu/Main_menu.tscn")
