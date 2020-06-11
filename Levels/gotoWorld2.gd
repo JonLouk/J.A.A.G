91ab232fa3e8604f2f5ce905d2701dac876c5b0a
athamour1 <thanosmour.tk@gmail.com>
2020-06-11 18:47:42 +0300

    Add rough transitions

---
+++ Levels/gotoWorld2.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "Player":
+			get_tree().change_scene("res://Levels/World2.tscn")
